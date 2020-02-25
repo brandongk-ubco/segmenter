7322552a67543896654d74479fa90b69d4ef8c9c
Brandon Graham-Knight <brandongk@gmail.com>
2020-02-24 18:10:27 -0800

    Creating bin directory and scripts to check completeness and combine results

--- run_all_classes.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import subprocess
-import os
-
-limitSeconds = int(os.environ.get("LIMIT_SECONDS", "3600"))
-
-for clazz in ["1", "2", "3", "4"]:
-  subprocess.check_call("CLASS=%s LIMIT_SECONDS=%s sbatch run.sh" % (clazz, limitSeconds), shell=True)
