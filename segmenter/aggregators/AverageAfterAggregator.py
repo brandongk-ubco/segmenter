5a5bfb75a9a58c08e28e7ad9fafcf99bf9090efd
Brandon Graham-Knight <brandongk@gmail.com>
2020-05-04 12:08:58 -0700

    Small fixes to aggregators.

--- segmenter/aggregators/AverageAfterAggregator.py
+++ segmenter/aggregators/AverageAfterAggregator.py
@@ -9,9 +9,9 @@ class AverageAfterAggregator(Aggregator):
 
     def thresholds(self):
         return np.append(
-            np.append(np.linspace(0., 0.35, num=8),
-                      np.linspace(0.36, 0.45, num=10)),
-            np.linspace(0.5, 0.95, num=10))
+            np.append(np.linspace(0., 0.30, num=7),
+                      np.linspace(0.31, 0.40, num=10)),
+            np.linspace(0.45, 0.95, num=11))
 
     def display_name(self):
         return "Average After Sigmoid"
